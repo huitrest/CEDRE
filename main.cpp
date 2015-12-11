304ce724e2400c9505c4ed94249ee66a3ff2954b
huitrest <retry.azer@laposte.net>
2015-12-11 14:26:16 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include "fenetre.h"
+
+int main(int argc, char *argv[])
+
+{
+    QApplication app(argc, argv);
+    Fenetre fenetre;
+    fenetre.show();
+    return app.exec();
+
+}
+
